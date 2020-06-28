dea054fbb588b97a3ebae92d557276dbc0cf91e7
qiaozhen <qiaozhenleve@163.com>
2020-06-28 15:39:01 +0800

    2.0

--- select.js
+++ select.js
@@ -1,140 +1,133 @@
-(function (root, factory) {
-	root.Select = factory();
-})(this, function () {
-	"use strict";
+(function ($) {
+	$.fn.extend({
+		Select: function (request) {
+			const select = this;
+			let inputValue = "";
+			let areaInformation = {
+				province: {
+					name: "",
+					id: "",
+				},
+				city: {
+					name: "",
+					id: "",
+				},
+				area: {
+					name: "",
+					id: "",
+				},
+			};
 
-	let Select = {};
+			let content = `<label>请选择地区</label><i id="choose_icon" class="layui-icon layui-icon-down "></i>`;
+			let choose = `<div class="choose"><div class="choose-hide"><div class="class province"><ul></ul></div><div class="class city"><ul><li><span><span></li></ul></div><div class="class area"><ul></ul></div></div></div>`;
+			$(select).append(content, choose);
 
-	Select.version = "0.1.0";
+			json.forEach((element) => {
+				let str = ` <li id="${element.code}"><span>${element.name}</span> <i class="layui-icon layui-icon-right"></i></li>`;
+				$(select).find(".province ul").append(str);
+			});
 
-	Select.areaInformation = {
-		province: {
-			name: "",
-			id: "",
-		},city: {
-			name: "",
-			id: "",
-		},area: {
-			name: "",
-			id: "",
-		}};
-	Select.inputValue;
-
-	Select.init = function (id, options) {
-		this.id = id;
-		let content = `<label>请选择地区</label><i id="choose_icon" class="layui-icon layui-icon-down "></i>`;
-		let choose = `<div class="choose"><div class="choose-hide"><div class="class province"><ul></ul></div><div class="class city"><ul><li><span><span></li></ul></div><div class="class area"><ul></ul></div></div></div>`;
-		$(this.id).append(content, choose);
-
-		json.forEach((element) => {
-			let str = ` <li id="${element.code}"><span>${element.name}</span> <i class="layui-icon layui-icon-right"></i></li>`;
-			$(this.id).find(".province ul").append(str);
-		});
-	};
-
-	Select.action = function (callback) {
-		// 显示省选项
-		$(this.id).on("click", function (event) {
-			event.stopPropagation();
-			$(this).find(".choose").slideDown(200);
-			$(this).find("#choose_icon").removeClass("layui-icon-down").addClass("layui-icon-up");
-		});
-
-		//选择省显示市
-		$(this.id).find(".province li").on("click", function (event) {
+			// 显示省选项
+			$(select).on("click", function (event) {
 				event.stopPropagation();
+				$(this).find(".choose").slideDown(200);
+				$(this).find("#choose_icon").removeClass("layui-icon-down").addClass("layui-icon-up");
+			});
 
-				let area = $(this).find("span").text();
-				let areaID = $(this).attr("id");
+			//选择省显示市
+			$(select).find(".province li").on("click", function (event) {
+					event.stopPropagation();
 
-				Select.areaInformation.province.name = area;
-				Select.areaInformation.province.id = areaID;
-				Select.areaInformation.city = Select.areaInformation.area = {};
+					let area = $(this).find("span").text();
+					let areaID = $(this).attr("id");
 
-				Select.inputValue = "";
-				Select.inputValue = area + "/";
-				$(Select.id).find("label").text(Select.inputValue);
-				$(Select.id).find(".city ul,.area ul").empty();
+					areaInformation.province.name = area;
+					areaInformation.province.id = areaID;
+					areaInformation.city = areaInformation.area = {};
 
-				json.forEach((element) => {
-					if (element.code == $(this).attr("id")) {
-						element.city.forEach((item) => {
-							let str = ` <li id="${item.code}"><span>${item.name}</span><i class="layui-icon layui-icon-right"></i></li>`;
-							$(Select.id).find(".city ul").append(str);
-						});
-					}
+					inputValue = "";
+					inputValue = area + "/";
+					$(select).find("label").text(inputValue);
+					$(select).find(".city ul,.area ul").empty();
+
+					json.forEach((element) => {
+						if (element.code == $(this).attr("id")) {
+							element.city.forEach((item) => {
+								let str = ` <li id="${item.code}"><span>${item.name}</span><i class="layui-icon layui-icon-right"></i></li>`;
+								$(select).find(".city ul").append(str);
+							});
+						}
+					});
+
+					$(select).find(".city li,.area li").removeClass("avtive");
+					$(select).find(".city").css("display", "inline-block");
+					$(select).find(".area").css("display", "none");
+					$(this).addClass("avtive").siblings().removeClass("avtive");
+
+					request(areaInformation);
 				});
 
-				$(Select.id).find(".city li,.area li").removeClass("avtive");
-				$(Select.id).find(".city").css("display", "inline-block");
-				$(Select.id).find(".area").css("display", "none");
-				$(this).addClass("avtive").siblings().removeClass("avtive");
+			//选择市显示区
+			$(select).find(".city").on("click", "li", function (event) {
+					event.stopPropagation();
+					let area = $(this).find("span").text();
+					let areaID = $(this).attr("id");
 
-				callback(Select.areaInformation);
-			});
+					areaInformation.city.name = area;
+					areaInformation.city.id = areaID;
+					areaInformation.area = {};
 
-		//选择市显示区
-		$(this.id).find(".city").on("click", "li", function (event) {
-				event.stopPropagation();
-				let area = $(this).find("span").text();
-				let areaID = $(this).attr("id");
-
-				Select.areaInformation.city.name = area;
-				Select.areaInformation.city.id = areaID;
-				Select.areaInformation.area = {};
-
-				$(Select.id).find(".area ul").empty();
-				$(Select.id).find(".area li").removeClass("avtive");
-				Select.inputValueText = Select.inputValue.split("/");
-				if (Select.inputValueText.length >= 3) {
-					Select.inputValue = Select.inputValueText[0] + "/";
-				}
-				Select.inputValue = Select.inputValue + $(this).find("span").text() + "/";
-				$(Select.id).find("label").text(Select.inputValue);
-				$(this).addClass("avtive").siblings().removeClass("avtive");
-				$(Select.id).find(".area").css("display", "inline-block");
-
-				json.forEach((element) => {
-					if (element.code == Select.areaInformation.province.id) {
-						element.city.forEach((city) => {
-							if (city.code == $(this).attr("id")) {
-								city.area.forEach((item) => {
-									let str = ` <li id="${item.code}"><span>${item.name}</span></li>`;
-									$(Select.id).find(".area ul").append(str);
-								});
-							}
-						});
+					$(select).find(".area ul").empty();
+					$(select).find(".area li").removeClass("avtive");
+					inputValueText = inputValue.split("/");
+					if (inputValueText.length >= 3) {
+						inputValue = inputValueText[0] + "/";
 					}
+					inputValue = inputValue + $(this).find("span").text() + "/";
+					$(select).find("label").text(inputValue);
+					$(this).addClass("avtive").siblings().removeClass("avtive");
+					$(select).find(".area").css("display", "inline-block");
+
+					json.forEach((element) => {
+						if (element.code == areaInformation.province.id) {
+							element.city.forEach((city) => {
+								if (city.code == $(this).attr("id")) {
+									city.area.forEach((item) => {
+										let str = ` <li id="${item.code}"><span>${item.name}</span></li>`;
+										$(select).find(".area ul").append(str);
+									});
+								}
+							});
+						}
+					});
+
+					request(areaInformation);
 				});
 
-				callback(Select.areaInformation);
+			// 选择区关闭下拉
+			$(select).find(".area").on("click", "li", function (event) {
+					event.stopPropagation();
+
+					let area = $(this).find("span").text();
+					let areaID = $(this).attr("id");
+
+					areaInformation.area.name = area;
+					areaInformation.area.id = areaID;
+
+					$(this).addClass("avtive").siblings().removeClass("avtive");
+					inputValue = inputValue + $(this).find("span").text();
+					$(select).find("label").text(inputValue);
+
+					$(select).find(".choose").hide();
+					$(select).find("#choose_icon").removeClass("layui-icon-up").addClass("layui-icon-down");
+
+					request(areaInformation);
+				});
+
+			$("html").click(function () {
+				$(select).find(".choose").hide();
+				$(select).find("#choose_icon").removeClass("layui-icon-up").addClass("layui-icon-down");
 			});
-
-		// 选择区关闭下拉
-		$(this.id).find(".area").on("click", "li", function (event) {
-				event.stopPropagation();
-
-				let area = $(this).find("span").text();
-				let areaID = $(this).attr("id");
-
-				Select.areaInformation.area.name = area;
-				Select.areaInformation.area.id = areaID;
-
-				$(this).addClass("avtive").siblings().removeClass("avtive");
-				Select.inputValue = Select.inputValue + $(this).find("span").text();
-				$(Select.id).find("label").text(Select.inputValue);
-
-				$(Select.id).find(".choose").hide();
-				$(Select.id).find("#choose_icon").removeClass("layui-icon-up").addClass("layui-icon-down");
-
-				callback(Select.areaInformation);
-			});
-
-		$("html").click(function () {
-			$(Select.id).find(".choose").hide();
-			$(Select.id).find("#choose_icon").removeClass("layui-icon-up").addClass("layui-icon-down");
-		});
-	};
-
-	return Select;
-});
+		},
+	});
+})(jQuery);
