a98a55f2847d28c7b0621131816d2f04769fc7a4
qiaozhen <qiaozhenleve@163.com>
2020-07-06 11:56:22 +0800

    2.3

--- select.js
+++ select.js
@@ -19,21 +19,37 @@
 			};
 			
 			let option = {
-				expandTrigger:'click', // click / mouseover
+                expandTrigger:'click', // click / mouseover
+                text:'请选择地区'
 			}
 			for(let i in config) option[i] = config[i];
             $(select).css('lineHeight',option.height).find('.choose').css('top', option.height)
             $(select).css(option);
             
 
-			let content = `<label>请选择地区</label><i id="choose_icon" class="iconfont icon-xiala "></i>`;
+			let content = `<label><span>${option.text}</span> <i id="Select_delete" class="iconfont icon-shanchu"></label></i><i id="choose_icon" class="iconfont icon-xiala "></i>`;
 			let choose = `<div class="choose"><div class="choose-hide"><div class="class province"><ul></ul></div><div class="class city"><ul><li><span><span></li></ul></div><div class="class area"><ul></ul></div></div></div>`;
 			$(select).append(content, choose);
 
 			json.forEach((element) => {
 				let str = ` <li id="${element.code}"><span>${element.name}</span> <i class="iconfont icon-fanhui"></i></li>`;
 				$(select).find(".province ul").append(str);
-			});
+            });
+            
+            
+            $(select).mouseenter(function () {
+                let text = $(select).find("label span");
+                if(text.text() != option.text){
+                    $(select).find('#Select_delete').css('opacity','1')
+                }else {
+                    console.log('已经是初始化状态');
+                    return false;
+                }
+            });
+
+            $(select).mouseleave(function () {
+                $(select).find('#Select_delete').css('opacity','0')
+             });
 
 			// 显示省选项
 			$(select).on("click", function (event) {
@@ -55,7 +71,7 @@
 
 					inputValue = "";
 					inputValue = area + "/";
-					$(select).find("label").text(inputValue);
+					$(select).find("label span").text(inputValue);
 					$(select).find(".city ul,.area ul").empty();
 
 					json.forEach((element) => {
@@ -92,7 +108,7 @@
 						inputValue = inputValueText[0] + "/";
 					}
 					inputValue = inputValue + $(this).find("span").text() + "/";
-					$(select).find("label").text(inputValue);
+					$(select).find("label span").text(inputValue);
 					$(this).addClass("avtive").siblings().removeClass("avtive");
 					$(select).find(".area").css("display", "inline-block");
 
@@ -107,10 +123,12 @@
 								}
 							});
 						}
-					});
+                    });
+                    
+                   
 
 					request(areaInformation);
-				});
+                });
 
 			// 选择区关闭下拉
 			$(select).find(".area").on("click", "li", function (event) {
@@ -124,13 +142,44 @@
 
 					$(this).addClass("avtive").siblings().removeClass("avtive");
 					inputValue = inputValue + $(this).find("span").text();
-					$(select).find("label").text(inputValue);
+					$(select).find("label span").text(inputValue);
 
 					$(select).find(".choose").hide();
 					$(select).find("#choose_icon").removeClass("icon-shouqi").addClass("icon-xiala");
 
 					request(areaInformation);
-				});
+                });
+                
+                // 清空
+                $(select).find("#Select_delete").click(function(event){
+                    event.stopPropagation();
+                    $(select).find("label span").text(option.text)
+                    
+                    areaInformation = {
+                        province: {
+                            name: "",
+                            id: "",
+                        },
+                        city: {
+                            name: "",
+                            id: "",
+                        },
+                        area: {
+                            name: "",
+                            id: "",
+                        },
+                    };
+
+                    $(select).find(".choose").hide();
+				    $(select).find("#choose_icon").removeClass("icon-shouqi").addClass("icon-xiala");
+                    $(select).find('#Select_delete').css('opacity','0')
+
+                    $(select).find(".city li,.area li,.province li").removeClass("avtive");
+					$(select).find(".city").css("display", "none");
+					$(select).find(".area").css("display", "none");
+                    
+                    request(areaInformation);
+                })
 
 			$("html").click(function () {
 				$(select).find(".choose").hide();
